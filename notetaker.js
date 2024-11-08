d0a49d237cca30733042cf7d66b3fc9e68c5fbc2
Mario Cabrera <yankeescab@gmail.com>
2024-11-07 22:20:28 -0500

    added function to handle generation of notes list

--- notetaker.js
+++ notetaker.js
@@ -11,32 +11,45 @@ let notes = [];
 updatePageContent();
 
 document.getElementById("add").addEventListener("click", addNote);
-document.getElementById("remove").addEventListener("click", removeNote);
+document.getElementById("delete").addEventListener("click", removeNote);
 
 /**
  * Function which sets up the logic to update the page based on the current notes
  */
 function updatePageContent() {
-  let noteList = document.getElementById("notelist");
   let optionList = document.getElementById("noteselection");
+
   let newOption = document.createElement("option");
-  let newNote = document.createElement("li");
 
   if (notes.length == 0) {
     document.getElementById("display-notes").classList.add("display-none");
     document.getElementById("delete-note").classList.add("display-none");
   } else {
-    document.getElementById("display-notes").classList.remove("display-none");
     document.getElementById("delete-note").classList.remove("display-none");
 
-    for (i = 0; i < notes.length; i++) {
+    for (let i = 0; i < notes.length; i++) {
+      noteList.innerHTML = "";
       newNote.textContent = notes[i];
       noteList.appendChild(newNote);
 
-      newOption.textContent = toString(i);
+      optionList.innerHTML = "";
+      newOption.textContent = notes[i];
       optionList.appendChild(newOption);
     }
   }
+}
+
+function updateNotesList() {
+  let noteList = document.getElementById("notelist");
+  let newNote = document.createElement("li");
+  noteList.innerHTML = "";
+
+  for (let i = 0; i < notes.length; i++) {
+    newNote.textContent = notes[i];
+    noteList.appendChild(newNote);
+  }
+
+  document.getElementById("display-notes").classList.remove("display-none");
 }
 
 /**
@@ -55,5 +68,6 @@ function addNote() {
 function removeNote() {
   let removeOption = document.getElementById("noteselection").value;
   notes.splice(removeOption, 1);
+  document.getElementById("noteselection").value = "";
   updatePageContent();
 }
